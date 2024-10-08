52c8e1d9d993fac8d126b2a1d28b9fc0189bf3e5
FefAzvdo <wfernandoaugusto01@gmail.com>
2024-10-08 16:27:30 -0300

    tailwind + shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <Button className="">Click me</Button>
+      <div className="flex items-center space-x-4 mt-5">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    </div>
   );
 }
 
