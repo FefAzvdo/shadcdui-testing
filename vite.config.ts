0abdaafa72a53406d8acd71d906682be21eba773
FefAzvdo <wfernandoaugusto01@gmail.com>
2025-01-28 14:16:05 -0300

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === "production" ? "/prod/" : "/",
-    define: {
-      "process.env.NODE_ENV": `"${mode}"`,
-    },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
-  };
+  },
 });
