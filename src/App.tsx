62b1aea3cf030209dbe241850fa0130e0dc736bc
FefAzvdo <wfernandoaugusto01@gmail.com>
2025-02-13 10:27:13 -0300

    ajustes fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,113 @@
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useStore } from "./store/bears";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+type Endereco = {
+  cep: string;
+  city: string;
+  neighborhood: string;
+  service: string;
+  state: string;
+  street: string;
+};
 
 function App() {
-  function BearCounter() {
-    const bears = useStore((state) => state.bears);
-    return <h1>{bears} bears around here...</h1>;
-  }
+  const [cep, setCep] = useState("");
+  const [dadosEndereco, setDadosEndereco] = useState<Endereco>();
+  const [loading, setLoading] = useState(false);
 
-  function Controls() {
-    const increasePopulation = useStore((state) => state.increasePopulation);
-    return <Button onClick={increasePopulation}>one up</Button>;
-  }
+  // Estado para controlar o tema
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Carregar o tema do localStorage ou usar o tema padrão
+  useEffect(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    if (savedMode === "true") {
+      setDarkMode(true);
+    }
+  }, []);
+
+  // Mudar o tema e salvar a preferência no localStorage
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("darkMode", newMode.toString());
+      return newMode;
+    });
+  };
+
+  const buscarCEP = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://brasilapi.com.br/api/cep/v1/${cep}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Erro ao buscar o CEP ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setDadosEndereco(data);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        alert(`Erro desconhecido ${error}`);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const formatarCEP = (cep: string) => {
+    return cep
+      .replace(/\D/g, "") // Remove tudo que não for número
+      .replace(/^(\d{5})(\d{0,3})/, "$1-$2") // Adiciona o hífen após os primeiros 5 dígitos
+      .slice(0, 9); // Garante que o CEP tenha no máximo 9 caracteres
+  };
+
   return (
-    <div className="p-4">
-      <BearCounter />
-      <Controls />
-      <p>Novo...</p>
-      <button
-        onClick={() => {
-          console.log(import.meta.env.VITE_API_URL);
-          console.log(import.meta.env.VITE_APP_ENV);
-        }}
-      >
-        LOG
-      </button>
-      <div className="flex items-center space-x-4 mt-5">
-        <Skeleton className="h-12 w-12 rounded-full" />
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-[250px]" />
-          <Skeleton className="h-4 w-[200px]" />
+    <div className={`${"w-screen h-screen"}  ${darkMode ? "dark" : "light"}`}>
+      <header>
+        <h1>Blog Post sobre Dark Mode em React</h1>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
+      </header>
+      <main>
+        <section>
+          <h2>O que é Dark Mode?</h2>
+          <p>
+            O Dark Mode é uma configuração de interface que troca a paleta de
+            cores para tons mais escuros, melhorando a leitura em ambientes com
+            pouca luz.
+          </p>
+        </section>
+        <div className="flex">
+          <input
+            style={{ background: darkMode ? "gray" : "white" }}
+            className="min-w-12 mr-4"
+            type="text"
+            placeholder="CEP"
+            value={formatarCEP(cep)}
+            onChange={(e) => setCep(e.target.value)}
+          />
+          <Button onClick={buscarCEP}>Pesquisar</Button>
         </div>
-      </div>
-      <p>VITE_APP_ENV = {import.meta.env.VITE_APP_ENV}</p>
+        {dadosEndereco &&
+          (loading ? (
+            <p className="mt-4">"Carregando..."</p>
+          ) : (
+            <ul className="mt-4">
+              {Object.entries(dadosEndereco).map((keyVal) => (
+                <p>
+                  {keyVal[0]} - {keyVal[1]}
+                </p>
+              ))}
+            </ul>
+          ))}
+      </main>
     </div>
   );
 }
